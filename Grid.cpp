8b2e3242882983de574ad45d9601c4cfa82e8019
Kobe Simoens <kobe.simoens@imbrsea.eu>
2025-08-15 14:10:51 +0200

    split up output

--- Grid.cpp
+++ Grid.cpp
@@ -171,35 +171,75 @@ void Grid::write_simulation(std::string w,double dt_init,double dt_sim){
 
 		netCDF::NcDim startDim = data.addDim("start", calc_ndays(NYEARSTART+YSTART)/DTSTART);
 		netCDF::NcDim partDim = data.addDim("particles", NPART);
-		netCDF::NcDim timeDim = data.addDim("time", NYEAR*365+1);
+		netCDF::NcDim yearsDim = data.addDim("years", NYEAR);
+		netCDF::NcDim daysDim = data.addDim("days", 365);
 
 		std::vector<netCDF::NcDim> dimVector;
 		dimVector.push_back(startDim);
-		dimVector.push_back(timeDim);
+		dimVector.push_back(yearsDim);
+		dimVector.push_back(daysDim);
 		dimVector.push_back(partDim);
 
 		std::vector<netCDF::NcDim> dimVector_start;
 		dimVector_start.push_back(startDim);
-		std::vector<netCDF::NcDim> dimVector_time;
-		dimVector_time.push_back(timeDim);
+		std::vector<netCDF::NcDim> dimVector_years;
+		dimVector_years.push_back(yearsDim);
+		std::vector<netCDF::NcDim> dimVector_days;
+		dimVector_days.push_back(daysDim);
+		std::vector<netCDF::NcDim> dimVector_init;
+		dimVector_init.push_back(startDim);
+		dimVector_init.push_back(partDim);
 
-		netCDF::NcVar timeVar = data.addVar("time", netCDF::ncInt, dimVector_time);
-		timeVar.putAtt("units", "days");
-		int vec_time[NYEAR*365+1];
-		for(int i=0;i<(NYEAR*365+1);i++){
-			vec_time[i] = i;
+		netCDF::NcVar yearsVar = data.addVar("years", netCDF::ncInt, dimVector_years);
+		yearsVar.putAtt("units", "years");
+		int vec_years[NYEAR];
+		for(int i=0;i<NYEAR;i++){
+			vec_years[i] = i;
 		}
-		std::vector<size_t> startp_time,countp_time;
-		startp_time.push_back(0);
-		countp_time.push_back(365*NYEAR+1);
-		timeVar.putVar(startp_time,countp_time,vec_time);
+		std::vector<size_t> startp_years,countp_years;
+		startp_years.push_back(0);
+		countp_years.push_back(NYEAR);
+		yearsVar.putVar(startp_years,countp_years,vec_years);
+
+		netCDF::NcVar daysVar = data.addVar("days", netCDF::ncInt, dimVector_days);
+		daysVar.putAtt("units", "days");
+		int vec_days[365];
+		for(int i=0;i<365;i++){
+			vec_days[i] = i;
+		}
+		std::vector<size_t> startp_days,countp_days;
+		startp_days.push_back(0);
+		countp_days.push_back(365);
+		daysVar.putVar(startp_days,countp_days,vec_days);
+
+		netCDF::NcVar initxVar = data.addVar("lon_0", netCDF::ncFloat, dimVector_init);
+		initxVar.putAtt("units", "radians");
+		netCDF::NcVar inityVar = data.addVar("mu_0", netCDF::ncFloat, dimVector_init);
+		inityVar.putAtt("units", "radians");
+		int vec_initx[calc_ndays(NYEARSTART+YSTART)/DTSTART][NPART];
+		int vec_inity[calc_ndays(NYEARSTART+YSTART)/DTSTART][NPART];
+		for(size_t i=0;i<calc_ndays(NYEARSTART+YSTART)/DTSTART;i++){
+			for(int j=0;j<NPART;j++){
+				vec_initx[i][j] = particles[i*NPART+j].getPathPos()[0].getX();
+				vec_inity[i][j] = particles[i*NPART+j].getPathPos()[0].getY();
+			}
+		}
+		std::vector<size_t> startp_init,countp_init;
+		startp_init.push_back(0);
+		startp_init.push_back(0);
+		countp_init.push_back(calc_ndays(NYEARSTART+YSTART)/DTSTART);
+		countp_init.push_back(NPART);
+		initxVar.putVar(startp_init,countp_init,vec_initx);
+		inityVar.putVar(startp_init,countp_init,vec_inity);
 
 		std::vector<size_t> startp,countp;
 		startp.push_back(0);
 		startp.push_back(0);
 		startp.push_back(0);
+		startp.push_back(0);
 		countp.push_back(1);
-		countp.push_back(NYEAR*365+1);
+		countp.push_back(1);
+		countp.push_back(365);
 		countp.push_back(NPART);
 
 		std::vector<size_t> startp_start,countp_start;
@@ -213,10 +253,10 @@ void Grid::write_simulation(std::string w,double dt_init,double dt_sim){
 			netCDF::NcVar posxVar = data.addVar("lon", netCDF::ncFloat, dimVector);
 			posxVar.putAtt("units", "radians");
 
-			float vec_part_posx[NYEAR*365+1][NPART];
+			float vec_part_posx[365][NPART];
 			netCDF::NcVar posyVar = data.addVar("mu", netCDF::ncFloat, dimVector);
 			posyVar.putAtt("units", "radians");
-			float vec_part_posy[NYEAR*365+1][NPART];
+			float vec_part_posy[365][NPART];
 		#endif
 
 		#ifdef STOREVEL
@@ -224,10 +264,10 @@ void Grid::write_simulation(std::string w,double dt_init,double dt_sim){
 
 			netCDF::NcVar velxVar = data.addVar("u", netCDF::ncFloat, dimVector);
 			velxVar.putAtt("units", "m/s");
-			float vec_part_velx[NYEAR*365+1][NPART];
+			float vec_part_velx[365][NPART];
 			netCDF::NcVar velyVar = data.addVar("v", netCDF::ncFloat, dimVector);
 			velyVar.putAtt("units", "m/s");
-			float vec_part_vely[NYEAR*365+1][NPART];
+			float vec_part_vely[365][NPART];
 		#endif
 
 		data.putAtt("stored data",store_str);
@@ -240,28 +280,36 @@ void Grid::write_simulation(std::string w,double dt_init,double dt_sim){
 		for(size_t i=0;i < (calc_ndays(NYEARSTART+YSTART)/DTSTART);i++){
 			vec_start[i] = i*DTSTART;
 			startp[0]=i;
-			for(int k=0;k < NPART;k++){
-				for(int j=0;j < (NYEAR*365+1);j++){
-					#ifdef STOREPOS
-						vec_part_posx[j][k] = particles[i*NPART+k].getPathPos()[j].getX();
-						vec_part_posy[j][k] = particles[i*NPART+k].getPathPos()[j].getY();
-					#endif
-					#ifdef STOREVEL
-						vec_part_velx[j][k] = particles[i*NPART+k].getPathVel()[j].getX();
-						vec_part_vely[j][k] = particles[i*NPART+k].getPathVel()[j].getY();
-					#endif
-				}				
-			}
 
-			#ifdef STOREPOS
-				posxVar.putVar(startp,countp,vec_part_posx);
-				posyVar.putVar(startp,countp,vec_part_posy);
-			#endif
-			#ifdef STOREVEL
-				velxVar.putVar(startp,countp,vec_part_velx);
-				velyVar.putVar(startp,countp,vec_part_vely);
-			#endif
+			for(int l=0;l<NYEAR;l++){
+				startp[1]=l;
+
+				for(int j=0;j<365;j++){
+
+					for(int k=0;k < NPART;k++){
+
+						#ifdef STOREPOS
+							vec_part_posx[j][k] = particles[i*NPART+k].getPathPos()[l*365+j+1].getX();
+							vec_part_posy[j][k] = particles[i*NPART+k].getPathPos()[l*365+j+1].getY();
+						#endif
+						#ifdef STOREVEL
+							vec_part_velx[j][k] = particles[i*NPART+k].getPathVel()[l*365+j+1].getX();
+							vec_part_vely[j][k] = particles[i*NPART+k].getPathVel()[l*365+j+1].getY();
+						#endif
+					}				
+				}
+
+				#ifdef STOREPOS
+					posxVar.putVar(startp,countp,vec_part_posx);
+					posyVar.putVar(startp,countp,vec_part_posy);
+				#endif
+				#ifdef STOREVEL
+					velxVar.putVar(startp,countp,vec_part_velx);
+					velyVar.putVar(startp,countp,vec_part_vely);
+				#endif
+			}
 		}
+
 		startVar.putVar(startp_start,countp_start,vec_start);
 
 		data.putAtt("length of trajectories",std::to_string(NYEAR)+" years");
